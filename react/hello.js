4564221735e28b5e595f56a8766e6acdb59c0cb3
Filipin <160949589+Filipe-marcelino@users.noreply.github.com>
2026-03-27 11:45:12 -0300

    Add files via upload

---
+++ react/hello.js
@@ -0,0 +1,44 @@
+// comando para rodar
+//node .\arquivo.js
+
+console.log('hello world');
+
+function somar() {
+    a+= 1
+}
+
+var a = 1; //variavel de escolpo global
+let b = 2; //variavel de escolpo local
+
+somar(a)
+console.log(a)
+
+for (var i=0; i<5; i++){
+
+}
+
+console.log(i)
+
+let c = true
+let d = false
+let e = true
+
+//c && d // and
+//c || d //or
+
+//c & d //and bitwise
+//c | d //or bitwise
+
+console.log(10 < i/0)
+
+let h = 15
+if (h < 18) {
+    console.log('Menor')
+} else {
+    console.log('Maior')
+}
+//sintaxe do if
+
+for (let p = 0; i < 5; p++) {
+    console.log(p)
+} //sintaxe de for: inicializa; faz o teste; depois entra na repeitição, p++ é igual p de um em um
