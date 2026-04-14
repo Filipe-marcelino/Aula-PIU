61f9ff39ec1279637b0284ea625939275c4c3f1f
Filipe <fmarcelino154@gmail.com>
2026-04-14 20:50:59 -0300

    meia questão, mas é avannço né?

---
+++ Treinojs/Q4.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let nome = prompt('Conta-me teu nome: ')
+let idade = Number(prompt('E quantos anos você tem?'))
+let disney = prompt('Você já visitou a disney (true/false)')
+disney = (disney.toLowerCase() === 'true')
+
+let dados = {
+    
+}
